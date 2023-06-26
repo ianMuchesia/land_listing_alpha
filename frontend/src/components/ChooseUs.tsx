b6c03805e0c5b865b3316bccca833c35205f9ac1
ianMuchesia <ianmuchesia@yahoo.com>
2023-06-26 17:55:37 +0300

    chooseus

--- frontend/src/components/ChooseUs.tsx
+++ frontend/src/components/ChooseUs.tsx
@@ -3,27 +3,27 @@ import { Icon } from '@iconify/react';
 
 const ChooseUs = () => {
   return (
-    <div className="features-container">
+    <div className="choose__us-container">
       <h3>Why Choose Us</h3>
-      <div className="features-grid">
-        <div className="feature-card">
-            <div className="feature-icon">
-            <Icon icon="carbon:group-security" />
+      <div className="choose__us-grid">
+        <div className="choose__us-card">
+            <div className="choose__us-icon">
+            <Icon icon="carbon:group-security" className='choose__icon' />
             </div>
           <h4>Reliable Agents</h4>
           <p>Our experienced agents are dedicated to helping you find the perfect land and guide you through the buying process.</p>
         </div>
-        <div className="feature-card">
-        <div className="feature-icon">
-        <Icon icon="mdi:neighbourhood" />
+        <div className="choose__us-card">
+        <div className="choose__us-icon">
+        <Icon icon="mdi:neighbourhood"  className='choose__icon'/>
         </div>
           <h4>Good Neighborhoods</h4>
           <p>We offer land listings in desirable neighborhoods known for their safety, amenities, and quality of life.</p>
         </div>
-        <div className="feature-card">
-        <div className="feature-icon">
+        <div className="choose__us-card">
+        <div className="choose__us-icon">
        
-        <Icon icon="eos-icons:pod-security" />
+        <Icon icon="eos-icons:pod-security" className='choose__icon' />
         </div>
           <h4>Housing Security</h4>
           <p>We prioritize your safety and ensure that all land listings adhere to proper legal regulations and security measures.</p>
